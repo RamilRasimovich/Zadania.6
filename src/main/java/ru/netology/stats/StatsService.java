3da960b8beb4e78ed52b39b2959a50047370b7b1
ramil506cx test <ramil506cx@icloud.com>
2023-04-17 01:40:46 +0500

    Gitignore added

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -11,9 +11,52 @@ public class StatsService {
 
     public long average(long[] sales) {
         return sum(sales) / sales.length;
+    }
 
+    public int monthMaximumSala(long[] sales) {
+        int montMaximum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[montMaximum]) {
+                montMaximum = i;
+            }
+
+        }
+        return montMaximum + 1;
 
     }
 
+    public int monthMinimumSala(long[] sales) {
+        int montMinimum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[montMinimum]) {
+                montMinimum = i;
+            }
+
+        }
+        return montMinimum + 1;
+    }
+
+    public int monthsBellowAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+
+    }
+    public int monthsAboveAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+
+    }
 
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -3,8 +3,6 @@ package ru.netology.stats;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class StatsServiceTest {
 
     @Test
@@ -27,4 +25,44 @@ class StatsServiceTest {
         Assertions.assertEquals(expected, actual);
 
     }
+    @Test
+    public void testMonthMaximumSala() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.monthMaximumSala(sales);
+        long expected = 8;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testMonthMinimumSala() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.monthMinimumSala(sales);
+        long expected = 9;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void monthsBellowAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.monthsBellowAverage(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void monthsAboveAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.monthsBellowAverage(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+
 }
